1db25afcda221c0ee6dce6d19f349f92b5e18d6e
ChenMengqi <hzchenmengqi@corp.netease.com>
2020-08-03 12:25:24 +0800

    [bug fix] 修复self被KVO监听的情况，object_getClassName(self)获取到isa替换后的类，导致crash的情况 Fixes #2

--- spa/Spa/SpaClass.m
+++ spa/Spa/SpaClass.m
@@ -299,7 +299,8 @@ static int callLuaFunction(lua_State *L, id self, SEL selector, NSInvocation *in
         int nresults = [signature methodReturnLength] ? 1 : 0;
         // get from table
         luaL_getmetatable(L, SPA_CLASS_LIST_TABLE);
-        lua_getfield(L, -1, object_getClassName(self));
+        //in case self KVO ,object_getClassName(self) get wrong class
+        lua_getfield(L, -1, [NSStringFromClass([self class]) UTF8String]);
         lua_getfenv(L, -1);
 
         if ([self class] == self) {
